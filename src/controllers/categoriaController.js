4fd4a743949247f9b54b8b8a65892a0d3eaf8b6d
Camila Raquel <raquelcamila909@gmail.com>
2026-06-23 12:59:39 -0300

    :recycle: refactor: centraliza tratamento de erros em middleware global

--- src/controllers/categoriaController.js
+++ src/controllers/categoriaController.js
@@ -3,72 +3,33 @@ const categoriaService = require('../services/categoriaService');
 class CategoriaController {
 
     async criar(req, res) {
-        try {
-            const {nome} = req.body;
-
-            const novaCategoria = await categoriaService.criar(nome);
-
-            return res.status(201).json(novaCategoria);
-        }
-        catch (error) {
-            const status = error.statusCode || 500;
-            return res.status(status).json({erro : error.message});
-        }
+        const {nome} = req.body;
+        const novaCategoria = await categoriaService.criar(nome);
+        return res.status(201).json(novaCategoria);
     }
 
     async listarTodos(req, res) {
-        try {
-            const categorias = await categoriaService.listarTodos();
-
-            return res.status(200).json(categorias);
-        }
-        catch (error) {
-            const status = error.statusCode || 500;
-            return res.status(status).json({erro: error.message});
-        }
+        const categorias = await categoriaService.listarTodos();
+        return res.status(200).json(categorias);
     }
 
     async atualizar(req, res) {
-        try {
-            const {id} = req.params;
-            const {nome} = req.body;
-            
-            const categoriaAtualizada = await categoriaService.atualizar(id, nome);
-
-            return res.status(200).json(categoriaAtualizada);
-        }
-        catch (error) {
-            const status = error.statusCode || 500;
-            return res.status(status).json({erro: error.message});
-        }
+        const {id} = req.params;
+        const {nome} = req.body;    
+        const categoriaAtualizada = await categoriaService.atualizar(id, nome);
+        return res.status(200).json(categoriaAtualizada);
     }
 
     async deletar(req, res) {
-        try {
-            const {id} = req.params;
-
-            await categoriaService.deletar(id);
-
-            return res.status(204).send();
-        }
-        catch (error) {
-            const status = error.statusCode || 500;
-            return res.status(status).json({erro: error.message});
-        }
+        const {id} = req.params;
+        await categoriaService.deletar(id);
+        return res.status(204).send();
     }
 
     async buscarPorId(req, res) {
-        try {
-            const {id} = req.params;
-
-            const categoriaEncontrada = await categoriaService.buscarPorId(id);
-
-            return res.status(200).json(categoriaEncontrada);
-        }
-        catch (error) {
-            const status = error.statusCode || 500;
-            return res.status(status).json({erro: error.message});
-        }
+        const {id} = req.params;
+        const categoriaEncontrada = await categoriaService.buscarPorId(id);
+        return res.status(200).json(categoriaEncontrada);
     }
     
 }
