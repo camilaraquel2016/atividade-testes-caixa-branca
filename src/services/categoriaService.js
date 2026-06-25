4ea84eb4c571ea0a0371b09adaa46631fecaa286
Camila Raquel <raquelcamila909@gmail.com>
2026-06-25 11:33:35 -0300

    :recycle: refactor(categoria): centraliza erros do banco com função utiliária withMappedError e reaproveita método de buscar por ID

--- src/services/categoriaService.js
+++ src/services/categoriaService.js
@@ -1,22 +1,21 @@
 const categoriaRepository = require("../repositories/categoriaRepository");
 const validador = require("../utils/validator");
 const DbErrors = require("../constants/dbErrors");
+const DbConstraints = require("../constants/dbConstraints");
+const { withMappedError} = require("../utils/errorHelper");
 
 class CategoriaService {
 
     async criar(nome) {
         validador.campoObrigatorio(nome, "O nome da categoria não pode ser nulo ou vazio");
 
-        try {
-            return await categoriaRepository.create(nome.trim());
-        } catch (dbError) {
-            if (dbError.code === DbErrors.UNIQUE_VIOLATION) {
-                const error = new Error("Já existe uma categoria cadastrada com este nome.");
-                error.statusCode = 409;
-                throw error;
+        return await withMappedError(
+            () => categoriaRepository.create(nome.trim()),
+
+            {
+                [DbConstraints.CATEGORIAS.NOME_UNIQUE]: {message: "Já existe uma categoria cadastrada com este nome.", statusCode: 409}
             }
-            throw dbError;
-        }
+        );
     }
 
     async listarTodos() {
@@ -27,92 +26,49 @@ class CategoriaService {
         validador.campoObrigatorio(id, "O ID da categoria não pode ser nulo ou vazio.");
         validador.campoObrigatorio(nome, "O novo nome da categoria não pode ser nulo ou vazio");
 
-        try {
-            const categoria = await categoriaRepository.findById(id);
-            if (!categoria) {
-                const error = new Error("Categoria não encontrada.");
-                error.statusCode = 404;
-                throw error;
+        await this.buscarPorId(id);
+
+        return await withMappedError(
+            () => categoriaRepository.update(id, nome.trim()),
+
+            {
+                [DbConstraints.CATEGORIAS.NOME_UNIQUE]: {message: "Já existe outra categoria cadastrada com este nome", statusCode: 409},
             }
-
-            return await categoriaRepository.update(id, nome.trim());
-        } catch (dbError) {
-            if (dbError.statusCode) throw dbError;
-
-            if (dbError.code === DbErrors.UNIQUE_VIOLATION) {
-                const error = new Error("Já existe outra categoria cadastrada com este nome");
-                error.statusCode = 409;
-                throw error;
-            }
-
-            if (dbError.code === DbErrors.INVALID_TEXT_REPRESENTATION) {
-                const error = new Error("Categoria informada não é válida");
-                error.statusCode = 400;
-                throw error;
-            }
-
-            throw dbError;
-        }
+        );
     }
 
     async deletar(id) {
         validador.campoObrigatorio(id, "O ID da categoria é obrigatório");
 
-        try {
-            const categoria = await categoriaRepository.findById(id);
-            if (!categoria) {
-                const error = new Error("Categoria não encontrada");
-                error.statusCode = 404;
-                throw error;
+        await this.buscarPorId(id);
+
+        return await withMappedError(
+            () => categoriaRepository.delete(id),
+
+            {
+                [DbErrors.FOREIGN_KEY_VIOLATION]: {message: "Não é possível deletar uma categoria que possui peças vinculadas" , statusCode: 400}
             }
-
-            return await categoriaRepository.delete(id);
-        } catch (dbError) {
-            if (dbError.statusCode) throw dbError;
-
-            if (dbError.code === DbErrors.FOREIGN_KEY_VIOLATION) {
-                const error = new Error("Não é possível deletar uma categoria que possui peças vinculadas.");
-                error.statusCode = 400;
-                throw error;
-            }
-
-            if (dbError.code === DbErrors.INVALID_TEXT_REPRESENTATION) {
-                const error = new Error("Categoria informada não é válida");
-                error.statusCode = 400;
-                throw error;
-            }
-
-            throw dbError;
-        }
+        );
     }
 
     async buscarPorId(id) {
-      validador.campoObrigatorio(id, 'O ID de categoria não pode ser nulo ou vazio.');
+        validador.campoObrigatorio(id, 'O ID de categoria não pode ser nulo ou vazio.');
 
-      try {
+        const categoriaEncontrada = await  withMappedError(
+            () => categoriaRepository.findById(id),
 
-        const categoriaEncontrada = await categoriaRepository.findById(id);
-
+            {
+                [DbErrors.INVALID_TEXT_REPRESENTATION]: {message: "ID de categoria informado não é válido", statusCode: 400}
+            }
+        );
+                
         if (!categoriaEncontrada) {
-          const error = new Error('Categoria não encontrada.');
-          error.statusCode = 404;
-          throw error;
+            const error = new Error('Categoria não encontrada.');
+            error.statusCode = 404;
+            throw error;
         }
 
         return categoriaEncontrada;
-      }
-      catch (dbError) {
-
-        if (dbError.statusCode) throw dbError;
-        
-        if (dbError.code === DbErrors.INVALID_TEXT_REPRESENTATION) {
-          const error = new Error('ID informado não é válido');
-          error.statusCode = 400;
-          throw error;
-        }
-
-        throw dbError;
-      }
     }
 }
 
