bf2df368a10719ead50013d8173833d1d8df616f
Ariele Macedo <arielemacedosilva823@gmail.com>
2026-06-26 18:19:27 -0300

    :recycle: refactor(pedido): remove DbErrors do service e transfere tratamento de erro para o middleware

--- src/routes/pedidoRoutes.js
+++ src/routes/pedidoRoutes.js
@@ -6,15 +6,16 @@ const pedidoController = require('../controllers/pedidoController');
 const validarRequisicao = require('../middlewares/validadorSchema')
 
 const { pedidoPostSchema, pedidoQuerySchema } = require('../schemas/pedidoSchema');
+const idSchema = require("../schemas/idSchema");
 
 router.post('/', validarRequisicao(pedidoPostSchema, "body"), pedidoController.criar);
 
 router.get('/', validarRequisicao(pedidoQuerySchema, "query"), pedidoController.listar);
 
-router.get('/:id', pedidoController.buscarPorId);
+router.get('/:id', validarRequisicao(idSchema, "params"), pedidoController.buscarPorId);
 
-router.patch('/:id', pedidoController.atualizarStatus);
+router.patch('/:id/status', validarRequisicao(idSchema, "params"), pedidoController.atualizarStatus);
 
-router.delete('/:id', pedidoController.deletar);
+router.delete('/:id', validarRequisicao(idSchema, "params"), pedidoController.deletar);
 
 module.exports = router;
