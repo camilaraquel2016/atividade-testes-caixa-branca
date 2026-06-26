fda0e09df651dc293314b6b237313c28084365a5
Ariele Macedo <arielemacedosilva823@gmail.com>
2026-06-26 17:00:31 -0300

    :sparkles: feat(cliente): adiciona validação de requisições com Zod

--- src/routes/clienteRoutes.js
+++ src/routes/clienteRoutes.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const clienteController = require('../controllers/clienteController');
 
-router.post('/', (req, res) => clienteController.criar(req, res));
+const clienteController = require('../controllers/clienteController')
+
+const validarRequisicao = require('../middlewares/validadorSchema')
+
+const { clientePostSchema, clientePatchSchema } = require('../schemas/clienteSchema');
+
+router.post('/', validarRequisicao(clientePostSchema, "body"), clienteController.criar);
+
 router.get('/', (req, res) => clienteController.listar(req, res));
 
+router.patch('/:id', validarRequisicao(clientePatchSchema, "body"), clienteController.atualizar);
+
 router.get('/:id', (req, res) => clienteController.buscarPorId(req, res));
-router.put('/:id', (req, res) => clienteController.atualizar(req, res));
+
 router.delete('/:id', (req, res) => clienteController.deletar(req, res));
 
 module.exports = router;
